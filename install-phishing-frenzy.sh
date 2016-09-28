89d9023aacbe3aa38c6efb984a37249f37479af3
0x776b7364 <dskw_86@yahoo.com>
2016-09-29 12:55:03 +1300

    Update for Kali 2016.2

--- install-phishing-frenzy.sh
+++ install-phishing-frenzy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# tested on Kali 201601 Rolling x86
-# working as of 21 Mar 2016
+# tested on Kali 2016.2 Rolling x86
+# working as of 29 Sep 2016
 # run this script as root
 # allow 45 minutes for installation
-# non-interactive except for Passenger installation portion (25 minutes from script execution) and Redis installation portion (45 minutes from script execution)
+# requires about 1.2GB of HDD space
+# non-interactive except for Passenger installation portion (20 minutes from script execution)
 # inspiration from:
 # - https://sathisharthars.wordpress.com/2014/07/02/installing-phishing-frenzy-in-kali-linux/
 # - https://www.phishingfrenzy.com/resources/install_kali_linux
@@ -13,6 +14,8 @@
 
 FQDN="phishing-frenzy.local"
 CURRENTIP="$(hostname -i)"
+RUBYVER="2.1.5"
+RAILSVER="4.2.5"
 
 echo "[*] Clone Repo …"
 git clone https://github.com/pentestgeek/phishing-frenzy.git /var/www/phishing-frenzy
@@ -21,8 +24,8 @@ echo "[*] Install RVM, Ruby and Packages …"
 \curl -sSL https://get.rvm.io | bash
 source /etc/profile.d/rvm.sh
 rvm pkg install openssl
-rvm install 2.1.5 --with-openssl-dir=/usr/local/rvm/usr
-rvm all do gem install --no-rdoc --no-ri rails
+rvm install $RUBYVER --with-openssl-dir=/usr/local/rvm/usr
+rvm all do gem install --no-rdoc --no-ri rails -v $RAILSVER
 rvm all do gem install --no-rdoc --no-ri passenger
 
 echo "[*] Install Passenger …"
@@ -30,11 +33,10 @@ apt-get update
 apt-get install -y apache2-dev libcurl4-openssl-dev
 leafpad /etc/apache2/apache2.conf &
 
-echo "The Passenger Apache module will be installed next. Follow the instructions to copy the LoadModule text stated into apache2.conf, then save the edits."
-echo "The Python option in the installation can be deselected."
+echo -e "\nThe Passenger Apache module will be installed next. Follow the instructions to copy the LoadModule text stated into apache2.conf, then save the edits."
 read -p "Press [Enter] key to continue."
 
-passenger-install-apache2-module
+passenger-install-apache2-module --languages ruby
 
 echo "[*] Apache VHOST Configuration …"
 echo >> /etc/apache2/apache2.conf
@@ -85,7 +87,7 @@ cd redis-stable/
 make
 make install
 cd utils/
-./install_server.sh
+echo -n | ./install_server.sh
 
 echo "[*] Sidekiq Configuration …"
 mkdir -p /var/www/phishing-frenzy/tmp/pids
@@ -106,4 +108,4 @@ echo "$CURRENTIP $FQDN"  >> /etc/hosts
 apachectl start
 
 echo "Login with admin : Funt1me!"
-iceweasel "http://$FQDN"
+/etc/alternatives/x-www-browser "http://$FQDN" &
